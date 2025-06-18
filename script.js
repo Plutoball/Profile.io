4bf2477e33d5e9753d505a9e3499a78266023889
Plutoball <likatshabalala@gmail.com>
2025-06-18 17:45:05 +0200

    adjust update

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scrolled');
     }
 });
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('kebabToggle');
+  const navMenu = document.getElementById('mobileMenu');
+
+  toggleBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+});
+// Close the mobile menu when a link is clicked
+const navLinks = document.querySelectorAll('#mobileMenu a');    
